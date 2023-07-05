0f75bc220b92a19df97c7ad29c2aebc325113443
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-07-05 18:24:23 +0300

    docs: Add more comments to explain each step better

--- api/src/routes/words.ts
+++ api/src/routes/words.ts
@@ -20,20 +20,28 @@ interface word {
  * @param res response object which responds with the words array
  */
 const generateRndWordsArry = (_req: Request, res: Response) => {
+  // create empty array to store the generated words
   const wordsArry: word[] = [];
+  // array of the 4 types of pos to make sure that all of them are included in the generated words array
   const posArry = ["noun", "verb", "adjective", "adverb"];
 
+  // loop to populate the words array with 10 items
   while (wordsArry.length < 10) {
     // generate a random word from data word list
     const randomWord = randomWordGenerator(data.wordList);
 
-    // loop inside the posArry to make sure that the final arary contains all 4 pos words
+    // loop inside the posArry to make sure that the final arary contains all 4 types of pos words
     posArry.forEach((el, i) => {
+      // if statement that check if the random generated word equals one of the 4 types of pos
+      // if true then remove that type from the pos array and add the word to the words array
       if (randomWord.pos === el) {
         posArry.splice(i, 1);
         wordsArry.push(randomWord);
       }
     });
+
+    // when the foreach loop finishes we will have 4 uniqe types of pos words inside the words array
+    // then after we can just add random generated words to the words array
     if (wordsArry.length >= 4) {
       wordsArry.push(randomWord);
     }
