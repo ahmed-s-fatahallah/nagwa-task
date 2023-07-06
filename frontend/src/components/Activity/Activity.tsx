7116131266093363897a0687d59d4f3d3a1f4a7e
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-07-06 17:54:36 +0300

    feat: Implement the logic for the activity

--- frontend/src/components/Activity/Activity.tsx
+++ frontend/src/components/Activity/Activity.tsx
@@ -1,23 +1,84 @@
+import { useEffect, useRef, useState } from "react";
 import MainBtn from "../MainBtn/MainBtn";
 
 import classes from "./Activity.module.css";
+
+interface word {
+  id: number;
+  word: string;
+  pos: string;
+}
+
 const Activity = () => {
+  const [words, setWords] = useState<word[]>([]);
+  const [wordNum, setWordNum] = useState(0);
+
+  useEffect(() => {
+    const fetchWords = async () => {
+      try {
+        const res = await fetch("http://localhost:8080/words");
+        const data = await res.json();
+        setWords(data);
+      } catch (error: unknown) {
+        if (error instanceof Error)
+          console.log(
+            `sorry an error has occured while getting the data ${error}`
+          );
+      }
+    };
+    fetchWords();
+  }, []);
+
+  const setwordNumHandler = () => {
+    setWordNum((prevState) => (prevState += 1));
+  };
+
+  let progBar;
+  if (words.length) progBar = (wordNum / words.length) * 100;
+
   return (
     <section className={classes["activity-section"]}>
-      <p className={classes.word}>Word</p>
-      <div className={classes["btns-container"]}>
-        <MainBtn text="Noun" />
-        <MainBtn text="Verb" />
-        <MainBtn text="Adverb" />
-        <MainBtn text="Adjective" />
-      </div>
+      <p className={classes.word}>{words[wordNum]?.word}</p>
+      <ul className={classes["btns-container"]}>
+        <li>
+          <MainBtn
+            text="Noun"
+            wordPos={words[wordNum]?.pos}
+            setWordNum={setwordNumHandler}
+          />
+        </li>
+        <li>
+          <MainBtn
+            text="Verb"
+            wordPos={words[wordNum]?.pos}
+            setWordNum={setwordNumHandler}
+          />
+        </li>
+        <li>
+          <MainBtn
+            text="Adverb"
+            wordPos={words[wordNum]?.pos}
+            setWordNum={setwordNumHandler}
+          />
+        </li>
+        <li>
+          <MainBtn
+            text="Adjective"
+            wordPos={words[wordNum]?.pos}
+            setWordNum={setwordNumHandler}
+          />
+        </li>
+      </ul>
       <div className={classes["prog-container"]}>
         <div className={classes["score-container"]}>
           <p>Score</p>
-          <span>50%</span>
+          <span>{`${progBar}%`}</span>
         </div>
         <div className={classes["prog-bar-container"]}>
-          <div className={classes["prog-bar"]}></div>
+          <div
+            className={classes["prog-bar"]}
+            style={{ width: `${progBar}%` }}
+          ></div>
         </div>
       </div>
     </section>
--- frontend/src/components/MainBtn/MainBtn.tsx
+++ frontend/src/components/MainBtn/MainBtn.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import classes from "./MainBtn.module.css";
 
 interface MainBtnProps {
   text: string;
+  wordPos: string;
+  setWordNum: () => void;
 }
 
 const MainBtn = (props: MainBtnProps) => {
+  const [btnClasses, setBtnClasses] = useState(classes.mainBtn);
+  const [score, setScore] = useState(0);
+
+  const onClickHandler = () => {
+    if (props.wordPos.toLocaleLowerCase() === props.text.toLocaleLowerCase()) {
+      setBtnClasses((prevState) => `${prevState} ${classes.correct}`);
+      setScore((prevState) => (prevState += 10));
+    } else {
+      setBtnClasses((prevState) => `${prevState} ${classes.wrong}`);
+    }
+    setTimeout(() => {
+      setBtnClasses(classes.mainBtn);
+      props.setWordNum();
+    }, 1000);
+  };
+
   return (
-    <button type="button" className={classes.mainBtn}>
+    <button type="button" className={btnClasses} onClick={onClickHandler}>
       {props.text}
     </button>
   );
