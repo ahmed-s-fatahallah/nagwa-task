a5f6eddd54030199b5296b3259e666bbb2a60ebc
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-07-08 15:58:27 +0300

    refactor: Remove the .env file and add url to network

--- frontend/src/network/index.ts
+++ frontend/src/network/index.ts
@@ -1,4 +1,4 @@
 export const ENDPOINTS = {
-  words: `${import.meta.env.VITE_API_ENDPOINT}/words`,
-  rank: `${import.meta.env.VITE_API_ENDPOINT}/rank`,
+  words: "http://localhost:8080/words",
+  rank: "http://localhost:8080/rank",
 } as const;
