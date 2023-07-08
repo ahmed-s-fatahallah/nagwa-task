c63f9b576ec51aef62ef85973154e168caf6584f
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-07-09 02:08:20 +0300

    refactor: Define API endpoint in a constant

--- frontend/src/network/index.ts
+++ frontend/src/network/index.ts
@@ -1,5 +1,6 @@
 // CONSTANT ENDPOINTS LINKS USED IN THE PROJECT
+const API_ENDPOINT = "http://localhost:8080";
 export const ENDPOINTS = {
-  words: "http://localhost:8080/words",
-  rank: "http://localhost:8080/rank",
+  words: `${API_ENDPOINT}/words`,
+  rank: `${API_ENDPOINT}/rank`,
 } as const;
